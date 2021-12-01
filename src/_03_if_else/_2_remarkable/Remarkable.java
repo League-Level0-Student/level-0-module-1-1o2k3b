c390a7bb27405d1d80620fdaf9f8ba1650d98537
student <student@ip-172-31-49-171.us-west-2.compute.internal>
2021-11-30 19:56:22 -0800

    Yay!5

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,16 +8,21 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-	String dad = "biker, surfer, and a really great dad";
-	String mom = "swiss, snowboarder, and a and really great mom";
-	String malu = "fun, annoying, and a really greatlittle sister";
+	String dad = "my dad, Mark is a biker, surfer, and a really great dad";
+	String mom = "my mom, Sandra is swiss, snowboarder, and a really great mom";
+	String malu = "my little sister, Malu is fun, annoying, and a really great little sister";
 			// 2. Ask the user to enter a name. Store their answer in a variable
-	String question = JOptionPane.showInputDialog("put in any of these names: dad, mom, and malu");
-	if (question.equals ("dad")); {
+	String question = JOptionPane.showInputDialog("put in this: family");
+	if (question.equals ("family")); {
 		JOptionPane.showMessageDialog(null, dad);
 	}
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+	if (question.equals ("family")); {
+		JOptionPane.showMessageDialog(null, mom);
+	}		
+		if (question.equals ("family")); {
+			JOptionPane.showMessageDialog(null, dad);
 	}
-}
 
+}
+}
--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,23 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set a password in a String variable
+	String pass = "91283465";
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	JOptionPane.showInputDialog("put in a secret message");
+	
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+	JOptionPane.showInputDialog("find the secret message by guessing the password");
 	// 4. If their guess matches the password, show them the secret message
 
+	if
+	
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
+}
 }
