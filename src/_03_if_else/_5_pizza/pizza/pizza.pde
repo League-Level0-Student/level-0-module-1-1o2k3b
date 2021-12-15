48a512aeb28047b73e50ee0154cd841b404bb7d4
student <student@ip-172-31-62-205.us-west-2.compute.internal>
2021-12-14 19:56:58 -0800

    bye!2

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,6 +1,11 @@
-
+PImage olive;
+PImage pepperoni;
+PImage mushroom;
 void setup() {
-size(500, 500);    
+size(500, 500); 
+olive = loadImage("olive.png");
+pepperoni = loadImage("pepperoni.png");
+mushroom = loadImage("mushroom.png");
 }
 void draw() {
  fill(#FAE203);
@@ -9,6 +14,9 @@ fill(#FF0320);
 circle(225, 225, 208);
 fill(#FAE203);
 circle(225, 225, 180);
+image(olive, 140, 170);
+image(pepperoni, 205, 135);
+image(mushroom, 190, 230);
 
 
 
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,14 +12,29 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle = JOptionPane.showInputDialog("what creature walks with four legs in the morning 2 legs in the afternoon and 3 legs at night");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+String answer = "a human";
+if(answer.equals (riddle)) {
+	score += 1;
+	JOptionPane.showMessageDialog(null, "you passed the sphinx, so now you have to steal the ring from the gollum");
+	else
+		score -= 1;
+		JOptionPane.showMessageDialog(null, "you never find the gollum because you solved the riddle incorrectly and the sphinx killed you");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
+JOptionPane.showInputDialog("the next, second and final riddle is: what is in my pocket");
+String answer2 = "a ring";
+if (answer.equals (answer2));
+JOptionPane.showMessageDialog("you got the master ring from gollum and now you have to destroy it at the cracks of doom without getting killed by sauron the dark lord");
+score+=2;
+else
+	score-=999;
+JOptionPane.showMessageDialog(null, "you we're killed by the gollum.");
 
 		// 2. Make a pop up to show the score.
 		
